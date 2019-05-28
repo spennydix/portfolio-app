cadaff89986a25ea317f6efd3dde3f76c08cb8f3
spennydix <spennydix@gmail.com>
2019-05-27 22:47:40 -0700

    logo work, and creating the work component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
