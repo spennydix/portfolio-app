a16f0c769153b84aeada2923f16184bbc22487f6
spennydix <spennydix@gmail.com>
2019-05-28 02:24:49 -0700

    nav is working, found some superellipse magic, created more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,19 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { WorkComponent } from './work/work.component';
 import { ProjectComponent } from './project/project.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
+
+
+
+const appRoutes: Routes = [
+  { path: 'work', component: WorkComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/work', pathMatch: 'full' },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,11 +28,19 @@ import { ProjectDetailsComponent } from './project-details/project-details.compo
     NavbarComponent,
     WorkComponent,
     ProjectComponent,
-    ProjectDetailsComponent
+    ProjectDetailsComponent,
+    ContactComponent,
+    AboutComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+    // other imports here
   ],
   providers: [],
   bootstrap: [AppComponent]
